34de57c8a8824a31ac54fd9ff8fc43946406e06c
E-Srgnk <e.srgnk@gmail.com>
2021-06-10 23:30:27 +0300

    implemented attack and roll for the main character.

--- SimpleGame2D/Assets/Scripts/Hero.cs
+++ SimpleGame2D/Assets/Scripts/Hero.cs
@@ -8,6 +8,9 @@ public class Hero : MonoBehaviour
     [SerializeField] private float speed = 3f;
     [SerializeField] private float jumpForce = 7f;
     private bool isGrounded = false;
+    private float timeAttack = 0f;
+    private int faceDirection = 1;
+    private float rollForce = 4f;
 
     private Rigidbody2D rb;
     private SpriteRenderer sprite;  
@@ -25,14 +28,18 @@ public class Hero : MonoBehaviour
         sprite = GetComponentInChildren<SpriteRenderer>();
         animator = GetComponent<Animator>();
     }
-
+    
     private void Update()
     { 
-        CheckGround();
+        timeAttack += Time.deltaTime;
 
+        CheckGround();
+        
         animator.SetFloat("AirSpeedY", rb.velocity.y);
 
-        if (isGrounded && Input.GetButtonDown("Jump")) Jump();
+        if (Input.GetButtonDown("Fire1") && timeAttack > 0.25f) Attack();
+        else if (Input.GetKeyDown("left shift")) Roll();
+        else if (isGrounded && Input.GetButtonDown("Jump")) Jump();
         else if (Input.GetButton("Horizontal")) Run();    
         else State = HeroState.Idle;
     }
@@ -42,6 +49,9 @@ public class Hero : MonoBehaviour
         Vector3 direction = transform.right * Input.GetAxis("Horizontal");
         transform.position = Vector3.MoveTowards(transform.position, transform.position + direction, speed * Time.deltaTime);
         sprite.flipX = direction.x < 0.0f;
+         Debug.Log("direction = " + (direction.x < 0.0f));
+         if (direction.x > 0.0f) faceDirection = 1;
+        else if (direction.x < 0.0f) faceDirection = -1;
 
         if (isGrounded) State = HeroState.Run;
     }
@@ -53,6 +63,18 @@ public class Hero : MonoBehaviour
         animator.SetBool("Grounded", isGrounded);
         rb.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
     }
+
+    private void Attack() 
+    {
+        animator.SetTrigger("Attack");
+        timeAttack = 0f;
+	}
+
+    private void Roll() 
+    {
+        animator.SetTrigger("Roll");
+        rb.velocity = new Vector3(faceDirection * rollForce, rb.velocity.y);
+	}
 
     private void CheckGround()
     {
